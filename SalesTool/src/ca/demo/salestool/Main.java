bb982727d1999a01b7cde3287f80a70e5cadfe6c
Darlington Matongo <darmatongo@gmail.com>
2014-09-16 12:53:17 +0200

    Initial Checkin of sales tool

---
+++ SalesTool/src/ca/demo/salestool/Main.java
@@ -0,0 +1,9 @@
+package ca.demo.salestool;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Excute me before calling SalesData");
+	}
+
+}
---
+++ SalesTool/src/ca/demo/salestool/SalesData.java
@@ -0,0 +1,5 @@
+package ca.demo.salestool;
+
+public class SalesData {
+
+}
